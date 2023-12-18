780ae6e1fa40f34e36b6400d092d409ee201423f
RaulPalomo <raul.palomo.7e7@itb.cat>
2023-12-18 13:13:38 +0100

    Update ej 12

---
+++ Disseny Modular/ej12.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DissenyModular
+{
+    class ex12
+    {
+        public static void Main()
+        {
+            const string FirstMsg = "Digues el nombre de litres consumits aquest mes";
+            int liters;
+            double result=6;
+            Console.WriteLine(FirstMsg);
+            liters=Convert.ToInt32(Console.ReadLine());
+            result= Calc(liters,result);
+            Console.WriteLine(result);
+        }
+        public static double Calc(int liters,double result)
+        {
+            if ( liters >50)
+            {
+                if( liters <=200)
+                {
+                    result += (liters-50)*0.1;
+                }
+                else if( liters > 200)
+                {
+                    result += 150 * 0.1;
+                    result += (liters - 200) * 0.3;
+                }
+            }
+            return result;
+        }
+    }
+}
